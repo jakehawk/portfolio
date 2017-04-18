535d6b659e938ad63cedc458b60836ebdd23f373
Jake Hawkinson <jakehawk1095@gmail.com>
2017-04-17 21:39:28 -0700

    swapped out old boilerplate react app for static portfolio page in progress.

--- app/app.jsx
+++
@@ -1,7 +0,0 @@
-var React 		= require('react'),
-		ReactDOM 	= require('react-dom');
-
-ReactDOM.render(
-	<h1>Jake Hawkinson</h1>,
-	document.getElementById('app')
-);
--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-// Create our app
-var app = express();
-
-app.use(express.static('public'));
-
-var port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Express server is up on port ${port}`));
